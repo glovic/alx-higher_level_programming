c474376329faf514c8e21c0b12cf3011eb088331
Victor Nwaogu <xtineviktor@gmail.com>
2024-01-08 12:03:46 -0800

    minor fixing

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return 'None'
-    else:
-        return my_list[idx]
 
+
+def replace_in_list(my_list, idx, element):
+
+    # check for invalid index positions, if found, don't modify the list
+    if not (0 <= idx < len(my_list)):
+        return my_list
+
+    for i in range(len(my_list)):
+        if i == idx:
+            # update the list with the replacement's value
+            my_list[i] = element
+
+            return my_list
