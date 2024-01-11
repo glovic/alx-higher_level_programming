13ad8073a251e1bd824cc67fff460978fe493f2d
Victor Nwaogu <xtineviktor@gmail.com>
2024-01-10 16:27:20 -0800

    minor fixing

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 
 def search_replace(my_list, search, replace):
     # Create a new list where each occurrence of 'search' in 'my_list' is replaced by 'replace'
-    new_replace = [replace if element == search else element for element in my_list]
+    new_replace = list(map(lambda element: replace if element == search else element, my_list))
     # Return the new list
     return new_replace
 
