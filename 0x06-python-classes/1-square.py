9582aecd0a8a7a1dd04975e3c0e8ce655487ecad
Victor Nwaogu <xtineviktor@gmail.com>
2024-01-23 18:17:41 -0800

    fixing typos

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+
+"""
+Defines a blueprint for a Square as part of the OOP tasks
+"""
+
+
 class Square:
-    """Represent a square."""
+    """
+    A blueprint for working with squares
+    """
 
     def __init__(self, size):
-        """Initialize a new Square.
+        """
+        Initializes the Square class
 
         Args:
-            size (int): The size of the new square.
+            size (Any): the size of the square
         """
-		self.__size = size
+        self.__size = size
