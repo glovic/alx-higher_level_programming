6b014ac43e20a7f459ceb6ba96fb57fa3623f862
Victor Nwaogu <xtineviktor@gmail.com>
2024-03-14 14:08:18 -0700

    eates the database hbtn_0d_2 and the user user_0d_2 (typo fixing)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,13 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
+-- This script creates the database 'hbtn_0d_2' and the user 'user_0d_2'
+
+-- create the database if it does not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- create the user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- grant user read only permissions on the 'htbn_0d_2' database
+GRANT USAGE, SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
+-- effect the change
 FLUSH PRIVILEGES;
